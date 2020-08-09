d3ba7b4807348d59ef8ccef6120290f8411d5977
Daniel Broadhurst <daniel@beedesignstudio.co.uk>
2020-08-09 22:05:05 +0100

    ADDED Route Group

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 Use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\User as ResourcesUser;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,29 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/images', 'ImageController@upload');
-
-Route::middleware('auth:api')->get('/user/{user}', function (User $user = null) {
-    return response()->json(User::where('id',$user->id)->with('userProfile')->get());
-});
-
 /* Provides Login endpoint for Passport */
 Route::post('/login', 'AuthController@Login');
 Route::post('/register', 'AuthController@Register');
-Route::middleware('auth:api')->post('/logout', 'AuthController@Logout');
-Route::middleware('auth:api')->delete('/delete-account/{user}', 'AuthController@DeleteAccount');
-
-/* View User with Clubs and Club Admin */
-Route::middleware('auth:api')->get('/user', 'UserController@index');
-/* User Join & Leave Club */
-Route::middleware('auth:api')->post('/join-cycling-club/{cyclingClub}', 'CyclingClubController@joinCyclingClub');
-Route::middleware('auth:api')->post('/leave-cycling-club/{cyclingClub}', 'CyclingClubController@leaveCyclingClub');
-/* User Join & Leave Club Event */
-Route::middleware('auth:api')->post('/attend-club-event/{id}', 'ClubEventController@attendClubEvent');
-Route::middleware('auth:api')->post('/leave-club-event/{id}', 'ClubEventController@leaveClubEvent');
 
 /* User Profile Group */
 Route::middleware('auth:api')->group(function () {
+    Route::get('/user/{user}', function (User $user = null) {
+        return response()->json(new ResourcesUser($user));
+    });
     Route::resource('user-profile', 'UserProfileController')->only([
         'store', 'update'
     ]);
@@ -46,9 +33,19 @@ Route::middleware('auth:api')->group(function () {
         'cycling-club' => 'CyclingClubController'
     ]);
     Route::get('/countries', 'CountriesController@Index');
+    Route::post('/logout', 'AuthController@Logout');
+    Route::delete('/delete-account/{user}', 'AuthController@DeleteAccount');
+    /* View User with Clubs and Club Admin */
+    Route::get('/user', 'UserController@index');
+    /* User Join & Leave Club */
+    Route::post('/join-cycling-club/{cyclingClub}', 'CyclingClubController@joinCyclingClub');
+    Route::post('/leave-cycling-club/{cyclingClub}', 'CyclingClubController@leaveCyclingClub');
+    /* User Join & Leave Club Event */
+    Route::post('/attend-club-event/{id}', 'ClubEventController@attendClubEvent');
+    Route::post('/leave-club-event/{id}', 'ClubEventController@leaveClubEvent');
 });
 
 Route::fallback(function(){
     return response()->json([
-        'message' => 'Page Not Found. If error persists, contact info@website.com'], 404);
+        'message' => 'Page Not Found. If error persists, contact daniel@cycling-club.com'], 404);
 });
